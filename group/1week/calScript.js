2c923357bc2b2300aae7f7dc2856444dce237d82
seoje <seoje@test.test>
2021-06-02 17:18:35 +0900

    수요일 학원 커밋

--- group/1week/calScript.js
+++ group/1week/calScript.js
@@ -1,12 +1,21 @@
 // alert("확인")
+
+/* 버튼 클릭시 출력창에 표시하게 해주는 함수 */
+function fordisplay(myvalue) {
+    document.getElementById("output").innerHTML+=myvalue
+}
+/* //버튼 클릭시 출력창에 표시하게 해주는 함수 */
+
+/* C버튼 클릭시 출력값을 0으로 만드는 함수 */
 function forclear() {
     document.getElementById("output").innerHTML="0";
 }
+/* // C버튼 클릭시 출력값을 0으로 만드는 함수 */
+
+/* %버튼 클릭시 백분율로 변환해주는 함수 */
 function perc() {
     var value = document.getElementById("output").innerHTML;
     value = value/100
     document.getElementById("output").innerHTML=value;
 }
-function fordisplay(myvalue) {
-    document.getElementById("output").innerHTML+=myvalue
-}
+/* //%버튼 클릭시 백분율로 변환해주는 함수 */
