e30322ee9c6be9d43800db31e16f0107aa37cfeb
seoje89 <seoje89@test.com>
2021-06-03 01:04:29 +0900

    수요일 그룹과제 커밋

--- group/1week/calScript.js
+++ group/1week/calScript.js
@@ -1,13 +1,13 @@
 // alert("확인")
 
 /* 버튼 클릭시 출력창에 표시하게 해주는 함수 */
-function fordisplay(myvalue) {
+function printDisplay(myvalue) {
     document.getElementById("output").innerHTML+=myvalue
 }
 /* //버튼 클릭시 출력창에 표시하게 해주는 함수 */
 
-/* C버튼 클릭시 출력값을 0으로 만드는 함수 */
-function forclear() {
+/* C버튼 클릭시 출력값을 0으로 리셋시키는 함수 */
+function reset() {
     document.getElementById("output").innerHTML="0";
 }
 /* // C버튼 클릭시 출력값을 0으로 만드는 함수 */
